06b76ae5340c289f420a602234ac2192ec3e59f5
Nada Youssef <nraslan101@gmail.com>
2024-03-22 20:21:48 +0200

    Footer Design Content

--- Graduation-Project-ITI-Etsy-Frontend/src/app/Components/SharedComponents/Header/Footer/footer/footer.component.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/app/Components/SharedComponents/Header/Footer/footer/footer.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import Tooltip from 'bootstrap/js/dist/tooltip'; 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit { 
+
+  contentOfTooltip : string = "Etsy’s 100% renewable electricity commitment includes the electricity used by the data centers that host Etsy.com, the Sell on Etsy app, and the Etsy app, as well as the electricity that powers Etsy’s global offices and employees working remotely from home in the US." ;
+  
+
+
+  ngAfterViewInit(): void {
+    // Initialize tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+      new Tooltip(tooltipTriggerEl); 
+    });
+  }
+  
+
+ 
+  
 
 }
