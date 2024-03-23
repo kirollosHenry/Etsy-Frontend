4f8c953e7ee36b1f00cdd2be2a46aa2781064925
Nada Youssef <nraslan101@gmail.com>
2024-03-23 03:31:44 +0200

    Cart Design

--- Graduation-Project-ITI-Etsy-Frontend/src/app/app.component.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './Components/SharedComponents/Header/Footer/footer/footer.component';
 import { HeaderComponent } from './Components/SharedComponents/Header/header/header.component';
+import { CartComponent } from './Components/cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeaderComponent],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
