f1044c9f2e524a5ecfe8a6a3ddac488d55d2246c
Nada Youssef <nraslan101@gmail.com>
2024-03-23 22:37:21 +0200

    Update cart

--- Graduation-Project-ITI-Etsy-Frontend/src/app/app.routes.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CartComponent } from './Components/cart/cart.component';
 import { ProductListComponent } from './Components/Products/product-list/product-list.component';
 import { ProductDetailsComponent } from './Components/Products/product-details/product-details.component';
+import { BaseCategoryComponent } from './Components/BaseCategory/base-category/base-category.component';
 
 export const routes: Routes = [
 
@@ -9,7 +10,8 @@ export const routes: Routes = [
     
     //{path:'home', component:HomeComponent, title:'Home'},
     //{path:'about', component:AboutComponent, title:'About'},
-    //{path:'Category', component:CategryComponent, title:'Category'},
+    {path:'BaseCategory', component:BaseCategoryComponent, title:'baseCategory'},
+    {path:'Category', component:BaseCategoryComponent, title:'Category'},
     {path:'Cart', component:CartComponent, title:'Cart'},
     {path:'Products', component:ProductListComponent, title:'Products'},
     {path:'Product/:id', component:ProductDetailsComponent, title:'Product Details'},
