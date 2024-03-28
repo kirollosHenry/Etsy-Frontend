95398ffe8e20f3cd780fc7a80ac1829bae625042
Nada Youssef <nraslan101@gmail.com>
2024-03-28 22:21:53 +0200

    test

--- Graduation-Project-ITI-Etsy-Frontend/src/app/Components/login/login.component.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/app/Components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { JsonPipe, NgFor } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import {
   FormBuilder,
   ReactiveFormsModule,
@@ -7,6 +7,7 @@ import {
   FormGroup,
   FormControl,
 } from "@angular/forms";
+
 import { ActivatedRoute, Router } from "@angular/router";
 import { AuthService } from "../../Services/Authentication/auth.service";
 import { Login } from "../../Models/Accout/login";
@@ -22,86 +23,107 @@ import { Register } from "../../Models/Accout/register";
   styleUrl: "./login.component.css",
 })
 export class LoginComponent implements OnInit {
-  email: string="";
-  password: string="";
+  email: string = "";
+  password: string = "";
   error!: string;
-  form!:FormGroup;
-  formRegister!:FormGroup;
+  form!: FormGroup;
+  formRegister!: FormGroup;
   activatedRoute: any;
   showRegistrationForm: boolean = false;
-  emailRegister:string = "";
-  userNameRegister:string = "";
-  passwordRegister:string =""
-  
- 
-  constructor(private authService: AuthService, private router:Router , private register :ResgisterService ) { }
-  ngOnInit(){
+  emailRegister: string = "";
+  userNameRegister: string = "";
+  passwordRegister: string = "";
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private register: ResgisterService
+  ) {} //    private modalService: NgbModal
+
+  ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
+      email: new FormControl("", Validators.required),
+      password: new FormControl("", Validators.required),
     });
 
     this.formRegister = new FormGroup({
-      emailRegister: new FormControl('', Validators.required),
-      passwordRegister: new FormControl('', Validators.required),
-      userNameRegister: new FormControl('', Validators.required)
-  });
+      emailRegister: new FormControl("", Validators.required),
+      passwordRegister: new FormControl("", Validators.required),
+      userNameRegister: new FormControl("", Validators.required),
+    });
   }
 
   Login() {
     var loginRequest = <Login>{};
-    loginRequest.email = this.form.controls['email'].value;
-    loginRequest.password = this.form.controls['password'].value;
+    loginRequest.email = this.form.controls["email"].value;
+    loginRequest.password = this.form.controls["password"].value;
 
     this.authService.login(loginRequest).subscribe({
       next: (response: LoginResult) => {
         if (response.isAuthenticated) {
           // Handle successful authentication
-          console.log('Login successful');
-          console.log('Token:',response.token);
-          
-          this.router.navigate(['/Category']);
+          console.log("Login successful");
+          console.log("Token:", response.token);
+
+          this.router.navigate(["/Category"]);
           // Redirect or perform other actions here
         } else {
-          this.error =  'An unknown error occurred';
+          this.error = "An unknown error occurred";
         }
       },
-      error: error => {
-        this.error = 'An error occurred while logging in';
-      }
-    });   
+      error: (error) => {
+        this.error = "An error occurred while logging in";
+      },
+    });
   }
 
   Register() {
     var Request = <Register>{};
-    Request.Email = this.formRegister.controls['emailRegister'].value;
-    Request.Password = this.formRegister.controls['passwordRegister'].value;
-    Request.Username= this.formRegister.controls['userNameRegister'].value;
+    Request.Email = this.formRegister.controls["emailRegister"].value;
+    Request.Password = this.formRegister.controls["passwordRegister"].value;
+    Request.Username = this.formRegister.controls["userNameRegister"].value;
 
     this.register.login(Request).subscribe({
       next: (response: LoginResult) => {
         if (response.isAuthenticated) {
           // Handle successful authentication
-          console.log('Login successful');
-          console.log('Token:',response.token);
-          
-          this.router.navigate(['/Category']);
+          console.log("Login successful");
+          console.log("Token:", response.token);
+
+          this.router.navigate(["/Category"]);
           // Redirect or perform other actions here
         } else {
-          this.error =  'An unknown error occurred';
+          this.error = "An unknown error occurred";
         }
       },
-      error: error => {
-        this.error = 'An error occurred while logging in';
-      }
-    });   
-
+      error: (error) => {
+        this.error = "An error occurred while logging in";
+      },
+    });
   }
-
 
   toggleForm() {
     this.showRegistrationForm = !this.showRegistrationForm;
-  
   }
-  
+
+  // ttt
+
+  @ViewChild("loginFormContent") loginFormContent!: ElementRef;
+  // modalRef!: NgbModalRef;
+
+  // openLoginModal(): void {
+  //   this.modalRef = this.modalService.open(this.loginFormContent, { ariaLabelledBy: 'modal-basic-title' });
+  // }
+
+  // closeModal(): void {
+  //   if (this.modalRef) {
+  //     this.modalRef.close('Close button clicked');
+  //   }
+  // }
+
+  // dismissModal(): void {
+  //   if (this.modalRef) {
+  //     this.modalRef.dismiss('Dismiss button clicked');
+  //   }
+  // }
 }
