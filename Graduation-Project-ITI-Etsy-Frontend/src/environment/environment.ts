3fd711c251abf782114bc0c7d5f893b04a306dc9
Zyad <ziadhakem7@gmail.com>
2024-03-29 19:33:12 +0200

    solve conflict in enviroment

--- Graduation-Project-ITI-Etsy-Frontend/src/environment/environment.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/environment/environment.ts
@@ -3,8 +3,4 @@ export const environment = {
     BaseCategoryApiUrl: 'http://16.171.140.138/estyservice/api/BaseCategory',
     CategoryApiUrl: 'http://16.171.140.138/estyservice/api/Category',
     OrderApiUrl: 'http://16.171.140.138/estyservice/api/Order'
-    ProductApiUrl: 'http://16.171.140.138/estyservice/api/Product',
-    BaseCategoryApiUrl: 'http://16.171.140.138/estyservice/api/BaseCategory',
-    CategoryApiUrl: 'http://16.171.140.138/estyservice/api/Category',
-    OrderApiUrl: 'http://16.171.140.138/estyservice/api/Order'
 }
