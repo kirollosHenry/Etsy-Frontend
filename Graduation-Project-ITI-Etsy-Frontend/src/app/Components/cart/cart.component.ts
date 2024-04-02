817f92c7cc728803ed286dae05f07d43b60ac3d6
AbanoubGirGis <abanoubgirgis677@gmail.com>
2024-04-03 01:07:38 +0200

    Cart List From Service and display from API done

--- Graduation-Project-ITI-Etsy-Frontend/src/app/Components/cart/cart.component.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/app/Components/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
+import { CartService } from '../../Services/Cart/cart.service';
+import { Subscription } from 'rxjs';
+import { GetAllCartDTO, ICartAPI } from '../../Models/cart';
 
 
 @Component({
@@ -11,16 +14,32 @@ import { TranslateModule, TranslateService } from "@ngx-translate/core";
 })
 export class CartComponent implements OnInit{
 
+  CustomerId:string = "9b1e9185-7f14-4ad0-83b5-69f4c6beb990";
+  sub! : Subscription;
+  CartsList:GetAllCartDTO[] = [];
+  NumberOfCarts!: number;
   lang: string = "en";
 
   constructor(
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private _CartService : CartService
   ) {}
 
   ngOnInit(): void {
       // Localization
       this.lang = localStorage.getItem("lang") || "en";
       this.translateService.use(this.lang);
-    
+
+      //GetAllCarts
+      this.sub = this._CartService.GetAllCarts(this.CustomerId).subscribe({
+        next: (CartsDateAPI: ICartAPI) => {
+          debugger;
+          this.CartsList = CartsDateAPI.entities;
+          this.NumberOfCarts = CartsDateAPI.count;
+        },
+        error: (response) => {
+          console.log(response);
+        },
+      })
     }
 }
