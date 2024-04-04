2b20df4107055c4220cdd31176b6645fb1ab2231
AbanoubGirGis <abanoubgirgis677@gmail.com>
2024-04-04 17:02:10 +0200

    Add Payment (Almost) done

--- Graduation-Project-ITI-Etsy-Frontend/src/app/Components/cart/cart.component.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/app/Components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
 import { CartService } from "../../Services/Cart/cart.service";
 import { Subscription } from "rxjs";
@@ -6,7 +6,8 @@ import { GetAllCartDTO, ICartAPI } from "../../Models/cart";
 import { ActivatedRoute } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-
+import { Payment } from "../../Models/payment";
+declare var paypal: any; 
 @Component({
   selector: "app-cart",
   standalone: true,
@@ -17,7 +18,7 @@ import { CommonModule } from "@angular/common";
 
 
 export class CartComponent implements OnInit {
-  CustomerId: string = "c8cc4212-b621-4a65-a363-c9cc677e5bac";
+  CustomerId: string = "83a1a42e-3e33-482d-843e-5c027bf951ad";
   sub!: Subscription;
   CartsList: GetAllCartDTO[] = [];
   NumberOfCarts!: number;
@@ -27,6 +28,13 @@ export class CartComponent implements OnInit {
   productId !: number ;
   quantityUserChoose!: number;
 
+  ////// PayPal //////
+  @ViewChild('paypalButtonContainer', { static: true }) paypalButtonContainer!: ElementRef;
+  amountPrice:number = 10; 
+  payment: Payment = {
+    totalPrice: this.amountPrice, 
+    response: ''
+  };
 
   constructor(
     private translateService: TranslateService,
@@ -39,14 +47,12 @@ export class CartComponent implements OnInit {
     this.lang = localStorage.getItem("lang") || "en";
     this.translateService.use(this.lang);
 
-    
     // Get parameters From Product Details 
     this.route.queryParams.subscribe((params) => {
       this.productId = params["productId"]; 
       this.quantityUserChoose = params["quantity"]; 
     });
 
-    
     //GetAllCarts
     this.sub = this._CartService.GetAllCarts(this.CustomerId).subscribe({
       next: (CartsDateAPI: ICartAPI) => {
@@ -58,16 +64,56 @@ export class CartComponent implements OnInit {
       },
     });
 
-
-
+    // PayPal //
+    paypal.Buttons(
+      { style: {
+        display: 'inline-block',
+        layout:'horizontal',
+        color:'blue',
+        shape:'rect',
+        label:'paypal',
+      },
+      
+        createOrder: (data: any, actions: any) => {
+          // console.log("Create Order *********************");
+          return actions.order.create({
+            purchase_units:[
+              {
+                amount:{
+                  value:this.amountPrice.toString(),
+                  currency_code:'USD',
+                  breakdown: {
+                    item_total: {
+                      currency_code: "USD",
+                      value: "10.00"
+                    }
+                  }
+                }
+              }
+            ]
+          });
+        },
+        onApprove: (data: any, actions: any) => {
+          // console.log("onApprove *********************");
+          return actions.order.capture().then((details: any) => {
+            // console.log('Payment details:', details);
+            // this.router.navigate(['/success']);
+          });
+        },
+        onError: (err: any) => {         
+          console.log('Error creating PayPal order:', err);
+        }
+      }
+    ).render(this.paypalButtonContainer.nativeElement);
   }
 
 
   //Select List 
-
   getNumbersArray(max: number): number[] {
     return Array.from({ length: max }, (_, index) => index + 1);
   }
+
+
 
   
 }
