746c15c6f18f7e0c5f4831c94014810081aa947b
Nada Youssef <nraslan101@gmail.com>
2024-04-15 03:00:02 +0200

    Add Server Nada (Abanoub & Nada)

--- Graduation-Project-ITI-Etsy-Frontend/src/environment/environment.ts
+++ Graduation-Project-ITI-Etsy-Frontend/src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    // 
     // LocalApiAccount:'http://localhost:28835/api/Account',
     // ProfileAPI:'http://localhost:28835/api/Profile',
     // AccountApiUrl: 'http://16.171.140.138/estyservice/api/Account',
@@ -8,31 +9,26 @@ export const environment = {
     // OrderApiUrl: 'http://16.171.140.138/estyservice/api/Order',
     // CartApiUrl: 'http://16.171.140.138/estyservice/api/Cart',
     // PaymentApiUrl: 'http://16.171.140.138/estyservice/api/payments'
-    LocalApiAccount:'https://etsyecommerce.somee.com/api/Account',
-    ProfileAPI:'https://etsyecommerce.somee.com/api/Profile',
-    AccountApiUrl: 'https://etsyecommerce.somee.com/api/Account',
-    ProductApiUrl: 'https://etsyecommerce.somee.com/api/Product',
-    BaseCategoryApiUrl: 'https://etsyecommerce.somee.com/api/BaseCategory',
-    CategoryApiUrl: 'https://etsyecommerce.somee.com/api/Category',
-    OrderApiUrl: 'https://etsyecommerce.somee.com/api/Order',
-    CartApiUrl: 'https://etsyecommerce.somee.com/api/Cart',
-    PaymentApiUrl: 'https://etsyecommerce.somee.com/api/payments'
 
-    // LocalApiAccount:'http://16.171.140.138/estyservice/api/Account',
-    // AccountApiUrl: 'http://16.171.140.138/estyservice/api/Account',
-    // ProductApiUrl: 'http://16.171.140.138/estyservice/api/Product',
-    // BaseCategoryApiUrl: 'http://16.171.140.138/estyservice/api/BaseCategory',
-    // CategoryApiUrl: 'http://16.171.140.138/estyservice/api/Category',
-    // OrderApiUrl: 'http://16.171.140.138/estyservice/api/Order',
-    // CartApiUrl: 'http://16.171.140.138/estyservice/api/Cart',
-    // PaymentApiUrl: 'http://16.171.140.138/estyservice/api/payments'
+    //Kirollos Server
+    // LocalApiAccount:'https://etsyecommerce.somee.com/api/Account',
+    // ProfileAPI:'https://etsyecommerce.somee.com/api/Profile',
+    // AccountApiUrl: 'https://etsyecommerce.somee.com/api/Account',
+    // ProductApiUrl: 'https://etsyecommerce.somee.com/api/Product',
+    // BaseCategoryApiUrl: 'https://etsyecommerce.somee.com/api/BaseCategory',
+    // CategoryApiUrl: 'https://etsyecommerce.somee.com/api/Category',
+    // OrderApiUrl: 'https://etsyecommerce.somee.com/api/Order',
+    // CartApiUrl: 'https://etsyecommerce.somee.com/api/Cart',
+    // PaymentApiUrl: 'https://etsyecommerce.somee.com/api/payments'
 
-    // LocalApiAccount:'http://etsyapi.runasp.net/api/Account',
-    // AccountApiUrl: 'http://etsyapi.runasp.net/api/Account',
-//     ProductApiUrl: 'http://etsyapi.runasp.net/api/Product',
-//     BaseCategoryApiUrl: 'http://etsyapi.runasp.net/api/BaseCategory',
-//     CategoryApiUrl: 'http://etsyapi.runasp.net/api/Category',
-//     OrderApiUrl: 'http://etsyapi.runasp.net/api/Order',
-//     CartApiUrl: 'http://etsyapi.runasp.net/api/Cart',
-//     PaymentApiUrl: 'http://etsyapi.runasp.net/api/payments'
+    //Nada Server
+    ProfileAPI:'https://etsyapi.runasp.net/api/Profile',
+    LocalApiAccount:'https://etsyapi.runasp.net/api/Account',
+    AccountApiUrl: 'https://etsyapi.runasp.net/api/Account',
+    ProductApiUrl: 'https://etsyapi.runasp.net/api/Product',
+    BaseCategoryApiUrl: 'https://etsyapi.runasp.net/api/BaseCategory',
+    CategoryApiUrl: 'https://etsyapi.runasp.net/api/Category',
+    OrderApiUrl: 'https://etsyapi.runasp.net/api/Order',
+    CartApiUrl: 'https://etsyapi.runasp.net/api/Cart',
+    PaymentApiUrl: 'https://etsyapi.runasp.net/api/payments'
 }
